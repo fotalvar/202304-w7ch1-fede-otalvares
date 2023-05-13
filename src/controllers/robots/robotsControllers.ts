197b925dd80729a68ef96332c431ca8c89bda8ca
nary lozano <narylozano@gmail.com>
2023-05-13 13:13:55 +0200

    Add getRobots controller

---
+++ src/controllers/robots/robotsControllers.ts
@@ -0,0 +1,12 @@
+import { type Request, type Response } from "express";
+import Robot from "../../database/models/Robots";
+
+export const getRobots = async (req: Request, res: Response) => {
+  try {
+    const robots = await Robot.find().exec();
+
+    res.status(200).json({ robots });
+  } catch {
+    res.status(404).json({ message: "404 Can't get robots" });
+  }
+};
