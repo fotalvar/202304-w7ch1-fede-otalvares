2d6ba5c1cf05906ff3783f3f7cef33ad212ec22b
Fede Otálvares <fnotalvares@gmail.com>
2023-05-16 23:28:22 +0200

    Fix cors allowed origins

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import morgan from "morgan";
 import robotsRouter from "../routers/robots/robotsRouters.js";
 import {
@@ -10,10 +11,16 @@ import userRouter from "../routers/users/usersRouter.js";
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  next();
-});
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://202304-w7ch1-fede-otalvares-front.netlify.app/",
+];
+
+const corsOptions: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+
+app.use(cors(corsOptions));
 
 app.disable("x-powered-by");
 
